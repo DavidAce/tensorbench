1fc2dc737e95a98b3c6a08f4257d29b8a5eb616d
DavidAce <aceituno@kth.se>
2020-09-11 13:39:08 +0200

    Testing cuda

--- source/general/nmspc_tensor_omp.h
+++ source/general/nmspc_tensor_omp.h
@@ -8,27 +8,23 @@
     #include <omp.h>
     #include <thread>
 #endif
+
 #include <unsupported/Eigen/CXX11/Tensor>
 #include <memory>
-
 namespace Textra::omp {
-#if !defined(EIGEN_USE_GPU)
 
-    #if defined(_OPENMP) && defined(EIGEN_USE_THREADS)
-        inline std::unique_ptr<Eigen::ThreadPool>       tp;
-        inline std::unique_ptr<Eigen::ThreadPoolDevice> dev;
-        inline void                                     setNumThreads(int num_threads) {
-            if(not tp) tp = std::make_unique<Eigen::ThreadPool>(num_threads);
-            if(not dev and tp) dev = std::make_unique<Eigen::ThreadPoolDevice>(tp.get(), num_threads);
-        }
-
-
-
-    #else
-        inline std::unique_ptr<Eigen::DefaultDevice> dev = std::make_unique<Eigen::DefaultDevice>() ;
-        inline void setNumThreads([[maybe_unused]] int num_threads) {
-            if(not dev) dev = std::make_unique<Eigen::DefaultDevice>();
-        }
-    #endif
+#if defined(_OPENMP) && defined(EIGEN_USE_THREADS)
+    inline std::unique_ptr<Eigen::ThreadPool>       tp;
+    inline std::unique_ptr<Eigen::ThreadPoolDevice> dev;
+    inline void                                     setNumThreads(int num_threads) {
+        if(not tp) tp = std::make_unique<Eigen::ThreadPool>(num_threads);
+        if(not dev and tp) dev = std::make_unique<Eigen::ThreadPoolDevice>(tp.get(), num_threads);
+    }
+#else
+    inline std::unique_ptr<Eigen::DefaultDevice> dev = std::make_unique<Eigen::DefaultDevice>() ;
+    inline void setNumThreads([[maybe_unused]] int num_threads) {
+        if(not dev) dev = std::make_unique<Eigen::DefaultDevice>();
+    }
 #endif
+
 }
